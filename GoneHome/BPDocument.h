c0cebc45cc1351900657cbe15484497e000c4356
Bruno Philipe <contact@brunophilipe.com>
2013-09-17 00:44:31 -0300

    Initial Commit

---
+++ GoneHome/BPDocument.h
@@ -0,0 +1,13 @@
+//
+//  BPDocument.h
+//  GoneHome
+//
+//  Created by Bruno Philipe on 9/17/13.
+//  Copyright (c) 2013 Bruno Philipe. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BPDocument : NSDocument
+
+@end
